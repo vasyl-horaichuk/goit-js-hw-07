31813c33bb8e11ad4bda29282f49ac6182f95be4
Vasyl Horaichuk <vg.createam@gmail.com>
2022-10-30 23:24:56 +0300

    Виконав завдання 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryListByClassName = document.querySelector('.gallery');
 
 const itemMarkup = galleryItems.map(({ preview, original, description }) =>
@@ -15,31 +13,30 @@ const itemMarkup = galleryItems.map(({ preview, original, description }) =>
       alt="${description}"
     />
   </a>
-</div>`).join('');
+</div>`).join('')
 
-galleryListByClassName.innerHTML += itemMarkup;
+galleryListByClassName.innerHTML += itemMarkup
 
 const onShowBigImage = event => { 
     event.preventDefault()
     
     if (event.target.nodeName !== "IMG") {
-        return;
+        return
     }
+  
+  const bigImageSrc = event.target.dataset.source
     
+  const modal = basicLightbox.create(`
+    <img src="${bigImageSrc}" width="800" height="600">
+`)
+  
+  modal.show()
+  
+  galleryListByClassName.addEventListener('keydown', event => { 
+    if (event.key === 'Escape') {
+      modal.close()
+    }
+  })
 }
 
 galleryListByClassName.addEventListener('click', onShowBigImage)
-console.log(galleryListByClassName);
-
-
-
-
-
-
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// Реалізація делегування на div.gallery і отримання url великого зображення.
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у 
-// проект посилання на мініфіковані(.min) файли бібліотеки.
-// // Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// // Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна 
-// із зображенням з прикладів бібліотеки basicLightbox.
