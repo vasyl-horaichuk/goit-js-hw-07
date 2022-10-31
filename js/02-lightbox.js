59e72c952107397c68b36d885a143e349b7651ff
Vasyl Horaichuk <vg.createam@gmail.com>
2022-10-31 12:55:52 +0300

    Done
    Home work task one, task two does. Interesting tasks. I like JavaScript.

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,15 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galleryByListName = document.querySelector('.gallery');
+const galleryListByClassName = document.querySelector('.gallery');
 
 const itemMarkup = galleryItems.map(({ preview, original, description }) => 
 `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}"/>
 </a>`).join('')
 
-galleryByListName.insertAdjacentHTML("beforebegin", itemMarkup)
+galleryListByClassName.insertAdjacentHTML("afterbegin", itemMarkup)
 
 new SimpleLightbox('.gallery a', {
-	captionDelay: 250
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+  animationSpeed: 500,
+  overlayOpacity:	0.7,
 })
